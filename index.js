e19c74f0fbe55c3241730e409ce97e99f4f1f131
Tom Boutell <tom@punkave.com>
2018-02-01 14:23:57 -0500

    Compatible with workflow

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = {
 
   moogBundle: {
     directory: 'lib/modules',
-    modules: [ 
+    modules: [
       'apostrophe-personas-areas',
       'apostrophe-personas-custom-pages',
       'apostrophe-personas-pages',
@@ -40,9 +40,13 @@ module.exports = {
     // to incorporate the persona when appropriate.
 
     self.expressMiddleware = function(req, res, next) {
+
       if (req.method !== 'GET') {
         return next();
       }
+
+      var workflow = self.apos.modules['apostrophe-workflow'];
+
       // If a user clicks on the official persona switcher, this always
       // changes their cookie immediately, no matter what the old setting was.
       // After that the persona prefix is sufficient so redirect to get
@@ -60,14 +64,34 @@ module.exports = {
 
       // Find the persona suggested by the URL prefix and adjust req.url
       // after capturing that information.
-      //
-      // TODO: upgrade to deal with locale prefixes and persona prefix localization.
+
       var urlPersona = _.find(self.personas, function(persona) {
-        var prefix = persona.prefix;
+        var prefix;
+        var liveLocale = workflow && workflow.liveify(req.locale);
+        var workflowPrefix = '';
+        if (workflow) {
+          prefix = persona.prefixes[liveLocale];
+          workflowPrefix = workflow.prefixes[liveLocale];
+        } else {
+          prefix = persona.prefix;
+        }
+        if ((req.url.substr(0, workflowPrefix.length) + '/') !== workflowPrefix + '/') {
+          // Unprefixed route like /login
+          workflowPrefix = '';
+        }
         if (prefix && (
-          (req.url.substr(0, prefix.length) + '/') === (prefix + '/')
+          (req.url.substr(workflowPrefix.length, prefix.length) + '/') === (prefix + '/')
         )) {
-          req.url = req.url.substr(prefix.length);
+          // The workflow prefix is really in the slug, but the
+          // persona prefix is not. So snip it out to let
+          // apostrophe find it in the database:
+          //
+          // /fr/auto/driving becomes /fr/driving
+          //
+          // If there is no workflow prefix the default empty
+          // string just turns /auto/driving into /driving which
+          // is also what we want
+          req.url = req.url.substr(0, workflowPrefix.length) + req.url.substr(workflowPrefix.length + prefix.length);
           return true;
         }
       });
@@ -101,7 +125,7 @@ module.exports = {
 
       if (req.session.persona && (!urlPersona)) {
         // Add the persona prefix to the URL and redirect.
-        return res.redirect(_.find(self.personas, { name: req.session.persona }).prefix + req.url);
+        return res.redirect(self.addPrefix(req, req.session.persona, req.url));
       }
 
       if (req.session.persona) {
@@ -115,6 +139,44 @@ module.exports = {
         // TODO must recognize all valid names for site, even
         // in a setup with many localized hostnames
         return req.get('Referrer') && (req.get('Referrer').indexOf(self.apos.baseUrl) === 0);
+      }
+
+    };
+
+    // Add the prefix for the given persona name to the given URL.
+    // In the presence of workflow, the persona "prefix" falls between
+    // the workflow prefix (already in the URL) and the rest of the URL,
+    // and varies based on locale.
+
+    self.addPrefix = function(req, persona, url) {
+
+      var workflow = self.apos.modules['apostrophe-workflow'];
+      var personas = self.apos.modules['apostrophe-personas'];
+      var liveLocale = workflow && workflow.liveify(req.locale);
+      var workflowPrefix = (liveLocale && workflow.prefixes[liveLocale]) || '';
+      if (req.url.substr(0, workflowPrefix.length) !== workflowPrefix) {
+        // Workflow prefix is not actually present, probably a route like /login
+        workflowPrefix = '';
+      }
+      var personaInfo = _.find(personas.personas, { name: persona });
+      var prefix = workflow ? personaInfo.prefixes[liveLocale] : personaInfo.prefix;
+
+      if (url.match(/^https?:/)) {
+        var parsed = require('url').parse(url);
+        parsed.pathname = prepend(parsed.pathname);
+        return require('url').format(parsed);
+      } else {
+        var result = prepend(url);
+        return result;
+      }
+      function prepend(path) {
+        var result = path.substr(0, workflowPrefix.length) + prefix;
+        var remainder = path.substr(workflowPrefix.length);
+        if (remainder.length) {
+          return result + remainder;
+        } else {
+          return result + '/';
+        }
       }
 
     };
