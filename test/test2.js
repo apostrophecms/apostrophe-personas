a6267768ae26d8e3253c94d8c6d0f76645300538
pj <starsinmypockets@gmail.com>
2018-03-17 16:47:20 -0400

    Intermediate. This is close. Need to figure out how to park new pages.

---
+++ test/test2.js
@@ -0,0 +1,228 @@
+var assert = require('assert');
+var _ = require('lodash');
+var request = require('request');
+var j = request.jar();
+var j1 = request.jar();
+
+describe('Personas Module', function() {
+
+  var apos;
+  
+  const basePath = "http://localhost:3000/";
+  const userAgents = {
+    desktop: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
+    googlebot: "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+    googleImageBot: "Googlebot-Image/1.0"
+  }
+
+  this.timeout(3000);
+  
+  after(function() {
+    apos.db.dropDatabase();
+  });
+
+  /// ///
+  // EXISTENCE
+  /// ///
+
+  it('1. Should be a property of the apos object', function(done) {
+    apos = require('apostrophe')({
+      testModule: true,
+      baseUrl: basePath,
+
+      modules: {
+        'apostrophe-pages': {
+          park: [
+            {slug: '/', type: 'home'},
+            {slug: '/tc-related', type: 'home', persona:'tc'},
+            {slug: '/2r-related', type: 'home', persona:'2r'},
+          ],
+          types: [
+            {
+              name: 'home',
+              label: 'Home'
+            },
+            {
+              name: 'testPage',
+              label: 'Test Page'
+            }
+          ]
+        },
+        'apostrophe-personas': {
+          personas: [
+            {   
+              name: 'tc',
+              label: 'tc',
+              prefix: '/tc'
+            },  
+            {   
+              name: '2r',
+              label: '2r',
+              prefix: '/2r'
+            }   
+          ]   
+        }
+      },
+      afterInit: function(callback) {
+        assert(apos.modules['apostrophe-personas']);
+        // Should NOT have an alias!
+        assert(!apos.personas);
+        return callback(null);
+      },
+      afterListen: function(err) {
+        assert(!err);
+        done();
+      }
+    });
+  });
+  
+  it('2. Test initial page load', function(done) {
+    const opts = {
+      url: basePath, 
+      jar: j, 
+      method: 'GET',
+      headers: {
+        Referrer: basePath
+      }
+    } 
+    
+    request(opts, (err, res) => {
+        assert(!err);
+        assert(res.statusCode === 200, 'req success');
+        assert(res.toJSON().body.indexOf('ello world') >= 0, 'Has default apostrophe homepage');
+        done();
+    })
+  });
+  
+  it('3. Pass persona as query should set persona on session', function(done) {
+    const opts = {
+      url: basePath, 
+      method: 'GET',
+      jar: j,
+      qs: {persona: '2r'},
+      headers: {
+        Referrer: basePath
+      }
+    } 
+
+    request(opts, (err, res) => {
+        assert(!err);
+        assert(res.statusCode === 200, 'req success');
+        assert(res.toJSON().body.indexOf('ello world') >= 0, 'Has default apostrophe homepage');
+        done();
+    });
+  });
+  
+  it('4. Subsequent visit should load persona path', function(done) {
+    const opts = {
+      url: basePath, 
+      method: 'GET',
+      jar: j,
+      headers: {
+        Referrer: basePath
+      }
+    } 
+
+    request(opts, (err, res) => {
+        assert(!err);
+        assert(res.statusCode === 200, 'req success');
+        assert(res.req.path === '/2r/', 'Redirects to response specific path');
+        done();
+    });
+  });
+  
+  it('5. New session loads generic home page', function(done) {
+    const opts = {
+      url: basePath, 
+      method: 'GET',
+      jar: j1,
+      headers: {
+        Referrer: basePath
+      }
+    } 
+
+    request(opts, (err, res) => {
+        assert(!err);
+        assert(res.statusCode === 200, 'req success');
+        assert(res.toJSON().body.indexOf('ello world') >= 0, 'Has default apostrophe homepage');
+        assert(res.req.path === '/', 'loads base path');
+        done();
+    });
+  });
+  
+  it('6. Visit persona page sets persona session', function(done) {
+    const opts = {
+      url: basePath+'tc-related',
+      method: 'GET',
+      jar: j1,
+      headers: {
+        Referrer: basePath
+      }
+    } 
+
+    request(opts, (err, res) => {
+        console.log("><>", err, res.statusCode)
+        assert(!err);
+        assert(res.statusCode === 200, 'req success');
+        done();
+    });
+  })
+
+  it('7. Subsequent request is forwarded to persona url', function(done) {
+    const opts = {
+      url: basePath,
+      method: 'GET',
+      jar: j1,
+      headers: {
+        Referrer: basePath
+      }
+    } 
+
+    request(opts, (err, res) => {
+        console.log('tcre', res.req.path);
+        assert(!err);
+        assert(res.statusCode === 200, 'req success');
+        assert(res.req.path === '/tc/', 'Redirects to response specific path');
+        done();
+    });
+  });
+  
+  it('8. Visit persona page loads default page, sets cookie', function(done) {
+    const url = basePath + '/2r-related';
+    const opts = {
+      url: basePath, 
+      method: 'GET',
+      qs: {persona: 'tc'},
+      jar: j1,
+      headers: {
+        Referrer: basePath
+      }
+    }
+
+    request(opts, (err, res) => {
+        assert(!err);
+        assert(res.statusCode === 200, 'req success');
+        assert(res.toJSON().body.indexOf('ello world') >= 0, 'Has default apostrophe homepage');
+        done();
+    });
+  });
+  
+  it('9. Subsequent visit to basePath redirects to persona base', function(done) {
+    const url = basePath;
+    const opts = {
+      url: basePath, 
+      method: 'GET',
+      jar: j1,
+      headers: {
+        Referrer: basePath
+      }
+    }
+
+    request(opts, (err, res) => {
+        assert(!err);
+        assert(res.statusCode === 200, 'req success');
+        assert(res.req.path === '/tc/', 'Redirects to persona specific path');
+        done();
+    });
+  });
+});
